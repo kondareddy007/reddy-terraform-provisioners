c4bd5ded764f465c7443c187d154b9349b2fc5e9
kondareddy <97215958+kondareddy007@users.noreply.github.com>
2024-09-26 13:48:57 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  #aws provider version not terraform version
+    }
+  }
+ 
+   backend "s3" {
+    bucket = "reddy1229-remote"
+    key    = "provisioner"
+    region = "us-east-1"
+    dynamodb_table = "reddy1229-locking"
+   } 
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
